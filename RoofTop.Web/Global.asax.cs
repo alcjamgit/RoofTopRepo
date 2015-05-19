c073681e69ae4dba11e0470a862eb3bf02e37977
alcjamgit <alcarazjames@gmail.com>
2015-05-19 10:36:04 +0800

    Include simple error logging

--- RoofTop.Web/Global.asax.cs
+++ RoofTop.Web/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using RoofTop.Web.App_Start;
 using RoofTop.Web.DependencyResolvers;
+using System.Net.Mail;
 
 namespace RoofTop.Web
 {
@@ -23,6 +24,27 @@ namespace RoofTop.Web
             ControllerBuilder.Current.SetControllerFactory(controllerFactory);
 
 
+        }
+
+        //Catch unhandled exceptions
+        void Application_Error(Object sender, EventArgs e)
+        {
+            var adminEmail = "james_alcaraz11@yahoo.com";
+            var exception = Server.GetLastError();
+            if (exception == null)
+                return;
+            var mail = new MailMessage { From = new MailAddress(adminEmail) };
+            mail.To.Add(new MailAddress(adminEmail));
+            mail.Subject = "Site Error at " + DateTime.Now;
+            mail.Body = "Error Description: " + exception.Message;
+            var server = new SmtpClient { Host = "smtp.mail.yahoo.com" };
+            server.Send(mail);
+
+            // Clear the error
+            Server.ClearError();
+
+            // Redirect to a landing page
+            //Response.Redirect("home/landing");
         }
     }
 }
